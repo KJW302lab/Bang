e3bacef8bf1abab8077b46c0008288874d973830
jiwoongKim <sandwich3777@gmail.com>
2025-08-20 14:34:35 +0900

    Player 객체 수정

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,40 +1,31 @@
 using Photon.Pun;
-using UnityEngine;
 
 public class Player : MonoBehaviourPun
 {
-    [SerializeField] private float moveSpeed = 2f;
-    [SerializeField] private float rotateSpeed = 1f;
-
-    public float MoveSpeed
-    {
-        get => moveSpeed;
-        set => moveSpeed = value;
-    }
-
-    public float RotateSpeed
-    {
-        get => rotateSpeed;
-        set => rotateSpeed = value;
-    }
-
+    public float moveSpeed = 2f;
+    public float rotateSpeed = 1f;
+    
     private PlayerView _view;
 
+    private int _health = 3;
+    
     private void Awake()
     {
         _view = gameObject.AddComponent<PlayerView>();
 
         if (photonView.IsMine)
-        {
-            gameObject.AddComponent<PlayerController>();
-            CameraManager.Instance.SetFollow(transform);
-        }
+            Initialize();
     }
 
+    private void Initialize()
+    {
+        gameObject.AddComponent<PlayerController>();
+        CameraManager.Instance.SetFollow(transform);
+    }
+    
     [PunRPC]
     private void GetHit()
     {
-        Debug.Log("눈덩이에 맞았습니다.");
         _view.PlayHit();
     }
 }
